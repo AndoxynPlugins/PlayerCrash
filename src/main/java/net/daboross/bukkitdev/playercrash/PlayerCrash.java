0f79bae32e65de9e0731e99e80f51c109258bea7
Dabo Ross <daboross@daboross.net>
2013-12-14 14:40:42 -0800

    Remove unused import

--- src/main/java/net/daboross/bukkitdev/playercrash/PlayerCrash.java
+++ src/main/java/net/daboross/bukkitdev/playercrash/PlayerCrash.java
@@ -2,7 +2,6 @@ package net.daboross.bukkitdev.playercrash;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
